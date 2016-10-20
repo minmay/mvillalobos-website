25c29d9936a6e508926c9e9068c3df82b606718d
minmay <mvillalobos@kineteque.com>
2016-10-20 01:00:42 -0700

    Refined the presentations link.

--- app/resume/presentations/presentations.component.ts
+++ app/resume/presentations/presentations.component.ts
@@ -4,6 +4,7 @@ import { ResumeService } from '../resume.service';
 import { Resume } from '../resume.model';
 import { Presentation } from '../presentation.model';
 import { OnInit } from '@angular/core';
+import { DomSanitizer, SafeResourceUrl, SafeUrl } from '@angular/platform-browser';
 
 @Component({
     selector: 'presentations',
@@ -13,9 +14,15 @@ import { OnInit } from '@angular/core';
 export class PresentationsComponent implements OnInit {
 
     presentations: Presentation[];
+    selectedPresentation: Presentation;
+    mediaUrl: SafeUrl;
+    showSlides: boolean;
+    showVideo: boolean;
 
-    constructor (private resumeService: ResumeService) {
-
+    constructor (
+        private resumeService: ResumeService,
+        private sanitizer: DomSanitizer
+    ) {
     }
 
     ngOnInit(): void {
@@ -26,5 +33,22 @@ export class PresentationsComponent implements OnInit {
         this.resumeService
             .findResume()
             .then(resume => this.presentations = resume.presentations);
+    }
+
+    onSelectPresentation(presentation: Presentation): void {
+        this.selectedPresentation = presentation;
+        this.onSelectSlides();
+    }
+
+    onSelectSlides() : void {
+        this.mediaUrl = this.sanitizer.bypassSecurityTrustResourceUrl(this.selectedPresentation.slides);
+        this.showSlides = true;
+        this.showVideo = false;
+    }
+
+    onSelectVideo(): void {
+        this.mediaUrl = this.selectedPresentation.video ? this.sanitizer.bypassSecurityTrustResourceUrl(this.selectedPresentation.video) : null;
+        this.showSlides = false;
+        this.showVideo = true;
     }
 }
