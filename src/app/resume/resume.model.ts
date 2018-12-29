a729904d4c318d9846cdbf2acea9f750e0449822
minmay <mvillalobos@kineteque.com>
2018-12-29 08:42:34 -0800

    Missing semicolon

--- src/app/resume/resume.model.ts
+++ src/app/resume/resume.model.ts
@@ -1,7 +1,7 @@
 import { Affiliation } from './affiliation.model';
 import { Employment } from './employment.model';
-import { Presentation } from './presentation.model'
-import { EducationEntry} from './education-entry.model'
+import { Presentation } from './presentation.model';
+import { EducationEntry} from './education-entry.model';
 
 export interface Resume {
     name: string;
