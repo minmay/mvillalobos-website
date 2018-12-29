f8d4fea2516626fcd61062fbc1fb6e1e71e7c00e
minmay <mvillalobos@kineteque.com>
2018-12-29 08:41:56 -0800

    An interface declaring no members is equivalent to its supertype.

--- src/app/resume/affiliation.model.ts
+++ src/app/resume/affiliation.model.ts
@@ -1,5 +1,11 @@
-import { ResumeEntry } from './entry/resume-entry.model';
-
-export interface Affiliation extends ResumeEntry {
-
+export interface Affiliation {
+  id: string;
+  name: string;
+  location: string;
+  logo: string;
+  start: string;
+  end: string;
+  title: string;
+  prologue: string;
+  accomplishments?: string[];
 }
